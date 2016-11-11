7f79fb79e7d27f1e021bc5775eb571976c0e6bd3
Jennifer Steffens <jensteff@users.noreply.github.com>
2016-11-11 11:30:59 -0600

    added a manual test train split

--- src/prediction.py
+++ src/prediction.py
@@ -4,6 +4,7 @@ from gensim.models import Word2Vec
 from vectorizer import vectorize
 from cleaning import clean
 import pandas as pd
+import numpy as np
 
 def prediction(vectorized_data):   
     headers = list(vectorized_data.columns.values)
@@ -13,7 +14,20 @@ def prediction(vectorized_data):
     predictor_columns = vectorized_data.drop('Number of Votes', 1)
     vector_columns = vectorized_data[vector_headers]
     
-    (train_data, test_data, train_target, test_target) =  ms.train_test_split(predictor_columns, target_column, test_size = 0.15)    
+    
+    vectorized_data.reindex(np.random.permutation(vectorized_data.index))
+    NUM_ROWS = vectorized_data.shape[0]
+    NUM_TEST = int(NUM_ROWS*.15)
+    
+    train_data = vectorized_data[NUM_TEST:]
+    train_target = train_data['Number of Votes']
+    train_data = train_data[vector_header]
+
+    test_data = vectorized_data[:NUM_TEST]
+    test_target = test_data['Number of Votes']
+    test_data = test_data[vector_header]
+    
+    #(train_data, test_data, train_target, test_target) =  ms.train_test_split(predictor_columns, target_column, test_size = 0.15)    
     
     classifier = RandomForestClassifier(n_estimators=10)
     classifier = classifier.fit(train_data[vector_headers], train_target)
